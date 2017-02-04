a38083002cf7b30de883175976000ce0887efa2a
chadrc <chadrcollins91@gmail.com>
2017-02-04 10:06:42 -0500

    Added idea project files.

--- Processor/src/external/java/com/chadrc/utils/http/ExternalRequest.java
+++ Processor/src/external/java/com/chadrc/utils/http/ExternalRequest.java
@@ -1,7 +1,29 @@
 package com.chadrc.utils.http;
 
+import java.util.Map;
+
 /**
  * Created by chad on 2/4/17.
  */
 public class ExternalRequest {
+
+    public ExternalRequest(String url) {
+
+    }
+
+    public void Get() {
+
+    }
+
+    public void Post(Object data) {
+
+    }
+
+    public void Put() {
+
+    }
+
+    public void Delete() {
+
+    }
 }
--- Processor/src/external/java/com/chadrc/utils/http/ExternalRequestParams.java
+++ Processor/src/external/java/com/chadrc/utils/http/ExternalRequestParams.java
@@ -4,4 +4,8 @@ package com.chadrc.utils.http;
  * Created by chad on 2/4/17.
  */
 public class ExternalRequestParams {
+
+    public ExternalRequestParams() {
+
+    }
 }
