8b66d4bac0a54a78d2a4b043a22ff3252a97b1fa
chadrc <chadrcollins91@gmail.com>
2017-02-12 16:45:50 -0500

    Updated build script with app boot up

--- build.sh
+++ build.sh
@@ -13,7 +13,29 @@ echo "Executing :App:build..."
 echo "Executing :App:publishToMavenLocal..."
 ../gradlew -q :App:publishToMavenLocal
 
-cd ../Client
-echo "Executing :Client:run..."
-../gradlew -q :Client:run
+echo "Starting :App:bootRun..."
+../gradlew -q :App:bootRun &
+
+echo "Waiting for connection..."
+SECONDS="0"
+while true; do
+    curl -s http://localhost:8080
+    if [ "$?" != "7" ]; then
+        echo "Connection made"
+        cd ../Client
+        echo "Executing :Client:run..."
+        ../gradlew -q :Client:run
+        break
+    elif [ "$SECONDS" -ge "10" ]; then
+        echo "Connection timeout"
+        break
+    fi
+    sleep 1
+    SECONDS=$[$SECONDS]
+    echo "${SECONDS} second(s) have passed"
+done
+
+echo "Will wait 5 seconds before stoping daemons."
+sleep 5
+../gradlew --stop
 
