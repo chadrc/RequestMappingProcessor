86a087c65439ce52241035df7d81fded655ab3e7
chadrc <chadrcollins91@gmail.com>
2017-02-12 18:25:54 -0500

    Corrected artifactId for RequestMappingProcessor Changed variable in test script so it doesn't double count Added 'Content-Type' to requests that have bodies

--- test.sh
+++ test.sh
@@ -5,7 +5,7 @@ echo "Starting :App:bootRun..."
 ../gradlew -q :App:bootRun &
 
 echo "Waiting for connection..."
-SECONDS="0"
+COUNTER="0"
 while true; do
     curl -s http://localhost:8080
     if [ "$?" != "7" ]; then
@@ -14,15 +14,15 @@ while true; do
         echo "Executing :Client:test..."
         ../gradlew :Client:test
         break
-    elif [ "$SECONDS" -ge "10" ]; then
+    elif [ "$COUNTER" -ge "10" ]; then
         echo "Connection timeout"
         break
     fi
     sleep 1
-    SECONDS=$((SECONDS+1))
-    echo "${SECONDS} second(s) have passed"
+    COUNTER=$((COUNTER + 1))
+    echo "${COUNTER} second(s) have passed"
 done
 
-echo "Will wait 5 seconds before stopping daemons."
-sleep 5
+echo "Will wait 10 seconds before stopping daemons."
+sleep 10
 ../gradlew --stop
