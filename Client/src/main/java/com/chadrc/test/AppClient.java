de170a9b05bab0d8cc287d8aa3733926a77bf52a
chadrc <chadrcollins91@gmail.com>
2017-02-12 12:03:33 -0500

    Added build configurations for testing ApiProperties

--- Client/src/main/java/com/chadrc/test/AppClient.java
+++ Client/src/main/java/com/chadrc/test/AppClient.java
@@ -1,11 +1,15 @@
 package com.chadrc.test;
 
+import com.chadrc.services.api.ApiProperties;
+import com.chadrc.test.models.responses.LoginResponse;
+
 /**
  * Created by chad on 2/11/17.
  */
 public class AppClient {
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        System.out.println("Root api url: " + ApiProperties.getRootUrl());
+        LoginResponse response = RootController.Login("chad", "password").getBody();
     }
 }
